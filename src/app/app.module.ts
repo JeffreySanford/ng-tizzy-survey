8698c0839a568f35d7e3292bbcfaef6dd80a21d1
Jeffrey Sanford <jeffreysanford@gmail.com>
2018-01-16 14:42:40 -0700

    styling and catagory to item selection model implimented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';  //<<<< import it here
 import { AppComponent } from './app.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import * as Survey from 'survey-angular';
 import { SurveyComponent } from './survey/survey.component';
 
-import { SurveyEditorComponent } from './survey-editor/survey-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SurveyComponent,
-    SurveyEditorComponent
   ],
   imports: [
-    BrowserModule
+    NgbModule.forRoot(),
+    BrowserModule,
+    FormsModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -1,19 +1,156 @@
 import { Component, OnInit } from '@angular/core';
 import * as Survey from '../../../node_modules/survey-angular';
 
-var surveyJSON = { title: "Tell us, what technologies do you use?", pages: [
-  { name:"page1", questions: [
-      { type: "radiogroup", choices: [ "Yes", "No" ], isRequired: true, name: "frameworkUsing",title: "Is Tizzy the sexist chatter in the Paltalk Education Section?" },
-      { type: "checkbox", choices: ["Smiley","UKChatmates","Meena"], hasOther: true, isRequired: true, name: "framework", title: "Who is the funniest chatter in the Paltalk Education Section?", visibleIf: "{frameworkUsing} = 'Yes'" }
-   ]},
-  { name: "page2", questions: [
-    { type: "radiogroup", choices: ["Yes","No"],isRequired: true, name: "mvvmUsing", title: "Do you use any MVVM framework?" },
-    { type: "checkbox", choices: [ "AngularJS", "KnockoutJS", "React" ], hasOther: true, isRequired: true, name: "mvvm", title: "What MVVM framework do you use?", visibleIf: "{mvvmUsing} = 'Yes'" } ] },
-  { name: "page3",questions: [
-    { type: "comment", name: "about", title: "Please tell us about your main requirements for Survey library" } ] }
- ]
-};
 
+var surveyJSON = {
+  triggers: [
+      {
+          type: "visible",
+          name: "type",
+          operator: "equal",
+          value: "one",
+          questions: ["one"]
+      }, {
+          type: "visible",
+          name: "type",
+          value: "two",
+          questions: ["two"]
+      }, {
+          type: "visible",
+          name: "type",
+          value: "three",
+          questions: ["three"]
+      }, {
+          type: "visible",
+          name: "type",
+          value: "four",
+          questions: ["four"]
+      }, {
+          type: "visible",
+          name: "type",
+          value: "five",
+          questions: ["five"]
+      }, {
+          type: "visible",
+          name: "type",
+          value: "six",
+          questions: ["six"]
+      }, {
+          type: "visible",
+          name: "type",
+          value: "seven",
+          questions: ["seven"]
+      }, {
+          type: "visible",
+          name: "type",
+          value: "eight",
+          questions: ["eight"]
+      }, {
+          type: "visible",
+          name: "type",
+          value: "other",
+          questions: ["otherType"]
+      }
+  ],
+  questions: [
+      {
+          type: "radiogroup",
+          name: "type",
+          isRequired: true,
+          colCount: 4,
+          hasOther: true,
+          title: "Please select the sport cars type.",
+          choices: [
+              "one",
+              "two",
+              "three",
+              "four",
+              "five",
+              "six",
+              "seven",
+              "eight"
+          ]
+      }, {
+          type: "radiogroup",
+          name: "one",
+          isRequired: true,
+          colCount: 4,
+          visible: false,
+          title: "please select an option",
+          hasOther: true,
+          choices: ["one", "two", "three", "four", "five"]
+      }, {
+          type: "radiogroup",
+          name: "two",
+          isRequired: true,
+          colCount: 4,
+          visible: false,
+          title: "please select an option",
+          hasOther: true,
+          choices: ["one", "two", "three", "four", "five"]
+      }, {
+          type: "radiogroup",
+          name: "three",
+          isRequired: true,
+          colCount: 4,
+          visible: false,
+          title: "please select an option",
+          hasOther: true,
+          choices: ["one", "two", "three", "four", "five"]
+      }, {
+          type: "radiogroup",
+          name: "four",
+          isRequired: true,
+          colCount: 4,
+          visible: false,
+          title: "please select an option",
+          hasOther: true,
+          choices: ["one", "two", "three", "four", "five"]
+      }, {
+          type: "radiogroup",
+          name: "five",
+          isRequired: true,
+          colCount: 4,
+          visible: false,
+          title: "please select an option",
+          hasOther: true,
+          choices: ["one", "two", "three", "four", "five"]
+      }, {
+          type: "radiogroup",
+          name: "six",
+          isRequired: true,
+          colCount: 4,
+          visible: false,
+          title: "please select an option",
+          hasOther: true,
+          choices: ["one", "two", "three", "four", "five"]
+      }, {
+          type: "radiogroup",
+          name: "seven",
+          isRequired: true,
+          colCount: 4,
+          visible: false,
+          title: "please select an option",
+          hasOther: true,
+          choices: ["one", "two", "three", "four", "five"]
+      }, {
+          type: "radiogroup",
+          name: "eight",
+          isRequired: true,
+          colCount: 4,
+          visible: false,
+          title: "please select an option",
+          hasOther: true,
+          choices: ["one", "two", "three", "four", "five"]
+      }, {
+          type: "comment",
+          name: "otherType",
+          title: "Please describe the car.",
+          isRequired: true,
+          visible: false
+      }
+  ]
+};
 @Component({
   selector: 'app-survey',
   templateUrl: './survey.component.html',
@@ -25,9 +162,26 @@ export class SurveyComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    var survey = new Survey.SurveyModel(surveyJSON);
-    // survey.onComplete.add(sendDataToServer);
-    Survey.SurveyNG.render("surveyElement", {model:survey});
-  }
+    Survey.Survey.cssType = "bootstrap";
+    Survey
+    .StylesManager
+    .applyTheme("red");
 
+    function sendDataToServer(survey) {
+      console.log(survey);
+      const resultAsString = JSON.stringify(survey.data);
+      alert(resultAsString); //send Ajax request to your web server.
+    }
+
+    var survey = new Survey.SurveyModel(surveyJSON);
+    survey.onComplete.add(sendDataToServer);
+    Survey.SurveyNG.render("surveyElement", { model: survey });
+
+    survey.onComplete
+      .add(function (result) {
+        document
+          .querySelector('#surveyResult')
+          .innerHTML = "result: " + JSON.stringify(result.data);
+    });
+  }
 }
